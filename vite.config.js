7fe5e19d6253505fb95127d36d49bce59db74996
vullagvs26 <smb.v.vullag@gmail.com>
2026-05-22 21:23:14 +0800

    configure vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/',
+  base: '/vincent-portfolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
